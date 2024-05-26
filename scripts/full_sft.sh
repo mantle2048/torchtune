2689d25743ef5f14b2d4d49285dc3509f49ffa88
my871432122 <871432122@qq.com>
2024-05-26 20:03:43 +0800

    add generate and full_sft scripts (taking tinyllama-alpaca as instance)

--- scripts/full_sft.sh
+++ scripts/full_sft.sh
@@ -2,29 +2,13 @@
 
 CKPT_PATH="/data/ckpts/TinyLlama-1.1B-Chat-v1.0"
 
-QUESTION="Please tell me a short story."
-
-PROMPT="<|system|>
-
-You are a friendly chatbot who always responds in the style of a pirate.</s>
-
-<|user|>
-
-${QUESTION}</s>
-
-<|assistant|>
-"
-
-tune run generate \
-  --config generation \
+tune run --nproc_per_node 1 full_finetune_distributed \
+  --config llama2/1.1B_full \
   model._component_="torchtune.models.llama2.tinyllama" \
   checkpointer.checkpoint_dir=$CKPT_PATH \
   checkpointer.checkpoint_files=["model.safetensors"] \
   checkpointer.output_dir=$CKPT_PATH \
   device="cuda" \
   tokenizer.path="${CKPT_PATH}/tokenizer.model" \
-  prompt="${PROMPT}" \
-  max_new_tokens="512" \
-  temperature="0.6" \
-  top_k="300" \
   dtype="fp32"
+
